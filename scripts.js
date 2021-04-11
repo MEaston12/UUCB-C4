df01bb872c32fbe087b065d3000fbfbe69fdef07
Malcolm Easton <eastonmalcolm@gmail.com>
2021-04-11 16:48:42 -0600

    added question population functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
+//magic numbers/vars
+const timerStart = 60;
+let timeLeft = timerStart;
+let questionStartTime = Date.now();
+let currentCard = '#welcome-card';
+let lastCard = '';
+let currentQuestion = 0;
+
 //Button assignments - Should stay relatively clean and refer to functions in the underworkings
 $('#start-quiz').click(()=>{
-    currentCard = '#quiz-card';
+    switchCard('#quiz-card');
+    startQuiz();
+    changeQuizCard(0);
+});
+
+$('#answers').find('button').click(()=>{
+    if(currentQuestion + 1 < questions.length){
+        submitAnswer();
+        changeQuizCard(++currentQuestion);
+    } else {
+        //Need to exit script and finish quiz
+        finishQuiz();
+        switchCard('#finish-card');
+    }
 });
 
 $('#view-high-scores').click(()=>{
@@ -13,15 +34,36 @@ $('#hide-high-scores').click(()=>{
     swapScoreButtons();
 });
 
-
 //Begin underworkings
-let currentCard = '#welcome-card';
-let lastCard = '';
-function switchCard(newCardStr){
+function startQuiz(){ //handles the mechanical changes needed to start the quiz
+    timeLeft = timerStart;
+    updateTimeLeft();
+}
+function finishQuiz(){
+
+}
+function submitAnswer(){
+
+}
+
+function changeQuizCard(qNum){
+    const questionObj = questions[qNum];
+    $('#question-num').text(qNum + 1);
+    $('#question-q').text(questionObj.question);
+    $('#answers').find('button').each((i, element)=>{ //iterate over button elements
+        element.textContent = questionObj.answers[i];
+    });
+}
+
+function switchCard(newCardStr){ //switches the visible card to a new card
     lastCard = currentCard;
     currentCard = newCardStr;
     $('.main-content').attr('hidden','');
     $(currentCard).attr('hidden',null);
+}
+
+function updateTimeLeft(){
+    $('#time-left').text() = timeLeft;
 }
 
 function swapScoreButtons(){
